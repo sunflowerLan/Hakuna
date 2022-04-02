9b8836be8016a441aa0c58df88ba1b8bc679ba94
zhanglanfang <zhanglanfang@zhanglanfangdeMacBook-Pro.local>
2022-04-02 17:46:34 +0800

    add case models

--- hakunaBE/cases/models.py
+++ hakunaBE/cases/models.py
@@ -7,7 +7,7 @@ class Module(models.Model):
     """
     模块表
     """
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, db_constraint=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField("名称", max_length=100, null=False, default="")
     parent_id = models.IntegerField("父级ID", default=0)
     is_delete = models.BooleanField("删除", default=False)
@@ -16,3 +16,15 @@ class Module(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+class Cases(models.Model):
+    """
+    用例表
+    """
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    name = models.CharField("名称", max_length=100, mull=False, default="")
+    priority = models.CharField("优先级", max_length=20, null=False, default="")
+    conditionsPre = models.TextField("前置条件", default="")
+    
+    create_time = models.DateTimeField("更新时间", auto_now_add=True)
+    update_time = models.DateTimeField("创建时间", auto_now=True)
